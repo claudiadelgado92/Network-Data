4c3beb4a619f2cf8e14c6ca5028e6d111d0ad883
DamienBabet <damien.babet@gmail.com>
2017-05-19 14:15:36 +0200

    Create nb_BooksInCommonV2.R
    version plus rapide, mais matrix non sorted

---
+++ Code_R/Util/nb_BooksInCommonV2.R
@@ -0,0 +1,19 @@
+nb_BooksInCommon = function(data.Ratings){
+  #INPUT  data.Ratings  : la base des notes
+  #OUTPUT mat.InCommon  : la matrice contenant le nombre de films not?s en commun pour chaque couple d'utilisateurs
+  
+  # formatage en graphe
+  g <- graph_from_data_frame(data.Ratings)
+  
+  # transformation en graphe bipartite avec l'attribut "type"
+  type <- (degree(g, mode="in")>0)
+  g <- set_vertex_attr(g,name="type", value=type)
+  cat(summary(V(g)$type))
+  
+  # Projection (pondérée) et récupération de la matrice d'adjacence
+  usersgraph <- bipartite_projection(g, multiplicity = T, which = F)
+  mat.InCommon <- get.adjacency(usersgraph, type="both", attr="weight", names=TRUE)
+
+  
+  return(matrix(mat.InCommon))
+}
